c78b5600e4185e12766138dc55739d6bbd941ab8
GarotoHonesto <luiginerysilva@outlook.com>
2022-05-18 18:42:42 -0300

    HenryCharacter

--- Assets/Main Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Main Project/Scripts/Player/PlayerMovement.cs
@@ -9,24 +9,27 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
 
     [Header("Velocidade")]
-    [SerializeField]
-    private float velocidade;
-    [SerializeField]
-    private float corrida;
+    public float velocidade = 0;
+    public float acceleration;
+    public float deceleration;
 
     [Header("Gravidade")]
     [SerializeField]
     private float gravidade = -9.81f;
 
+    public float rotationSpeed;
+
     private Vector3 movimentoInput;
     private Vector3 velocity;
-    private bool run;
+    public bool walk;
+    public bool run;
+    public bool idle;
     public int currentDirection = 1;
     private int futureDirection = 1;
     public float cameraChangeControlDelay;
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -48,14 +51,66 @@ public class PlayerMovement : MonoBehaviour
                 movimentoInput = new Vector3(-Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
                 break;
         }
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            run = true;
+            if (walk)
+            {
+                run = true;
+            }
         }
         else
         {
             run = false;
         }
+        
+        if(!run)
+        {
+           if(velocidade >= 3)
+            {
+                velocidade = 3;
+            }
+        }
+        else if(run)
+        {
+            if (velocidade >= 6)
+            {
+                velocidade = 6;
+            }
+        }
+        if(velocidade <= 0)
+        {
+            velocidade = 0;
+        }
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) && !run)
+        {
+            if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) && run)
+            {
+                velocidade += Time.deltaTime * acceleration;
+                idle = false;
+                walk = false;
+                run = true;
+            }
+
+            velocidade += Time.deltaTime * acceleration;
+            idle = false;
+            walk = true;
+            run = false;
+        }
+        else
+        {
+            velocidade -= Time.deltaTime * deceleration;
+            idle = true;
+            walk = false;
+            run = false;
+        }
+
+        if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            velocidade -= Time.deltaTime * deceleration;
+        }
+
         Movimento();
     }
     private void Movimento()
@@ -70,15 +125,18 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y -= gravidade * -2f * Time.deltaTime;
         }
-        if (run)
-        {
-            controller.Move(MoveVector * corrida * Time.deltaTime);
-        }
-        else
+        if (idle || walk || run)
         {
             controller.Move(MoveVector * velocidade * Time.deltaTime);
         }
         controller.Move(velocity * Time.deltaTime);
+
+        if(MoveVector != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(MoveVector, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
     public void setFutureDirection(int mov)
     {
