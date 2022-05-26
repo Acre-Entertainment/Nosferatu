b961defe582fbf121b09400c849e21b0c0d41e53
Fecyrineu <fcyrineu@gmail.com>
2022-05-25 22:33:35 -0300

    Chests

---
+++ Assets/Main Project/Scripts/Puzzles/Onde Dorme/CM_Chest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_Chest : MonoBehaviour
+{
+    InfoKeeper InfoKeeper;
+    public int keyNumber;
+
+    private void Start()
+    {
+        InfoKeeper = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<InfoKeeper>();
+    }
+
+    public void activateThis()
+    {
+        switch(keyNumber)
+        {
+            case 1: InfoKeeper.hasDormeKey1 = true; break;
+            case 2: InfoKeeper.hasDormeKey2 = true; break;
+            case 3: InfoKeeper.hasDormeKey3 = true; break;
+            case 4: InfoKeeper.hasDormeKey4 = true; break;
+        }
+    }
+}
