c53b582c39f80b5e4b0ee7c050d845d8d5085218
GarotoHonesto <luiginerysilva@outlook.com>
2022-05-31 02:23:55 -0300

    CheckPointPart2 & Porta Section8 Arrumada

--- Assets/Main Project/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Main Project/Scripts/Checkpoint/Checkpoint.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Checkpoint : MonoBehaviour
 {
     public GameObject player;
     public GameObject[] checkPoints;
     public Vector3 point;
-    private InfoKeeper infoKeeper;
+    private int newCheckPoint;
     // Start is called before the first frame update
     void Start()
-    {
-        infoKeeper = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<InfoKeeper>();
+    {        
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(newCheckPoint == 0)
+        {
+            point = new Vector3(18.7f, 7f, -56f);
+        }
+        else if(newCheckPoint == 1)
+        {
+            point = checkPoints[0].transform.position;
+        }
+        else if(newCheckPoint == 2)
+        {
+            point = checkPoints[1].transform.position;
+        }
+        else if (newCheckPoint == 3)
+        {
+            point = checkPoints[2].transform.position;
+        }
     }
 
     public void Spawn()
@@ -27,16 +43,16 @@ public class Checkpoint : MonoBehaviour
 
     public void Checkpoint1()
     {
-        point = checkPoints[0].transform.position;
+        newCheckPoint = 1;
     }
 
     public void Checkpoint2()
     {
-        point = checkPoints[1].transform.position;
+        newCheckPoint = 2;
     }
 
     public void Checkpoint3()
     {
-        point = checkPoints[2].transform.position;
+        newCheckPoint = 3;
     }
 }
